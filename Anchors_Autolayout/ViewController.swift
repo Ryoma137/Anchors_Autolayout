f290d08ef97d6dbd24a15c02d518cf5513df5e31
Ryoma Ishizaki <baz4071@gmail.com>
2021-02-04 20:22:40 +0900

    added labels

--- Anchors_Autolayout/ViewController.swift
+++ Anchors_Autolayout/ViewController.swift
@@ -19,11 +19,24 @@ class ViewController: UIViewController {
     func setUpViews(){
         
         let upperLeftLabel = makeLabel(withText: "upperLeft")
+        let upperRightLabel = makeLabel(withText: "upperRight")
+        let underLeftLabel = makeLabel(withText: "underLeft")
+        let underRightLabel = makeLabel(withText: "underRight")
+        
         view.addSubview(upperLeftLabel)
-        upperLeftLabel.topAnchor.constraint(equalTo: view.topAnchor,constant: 8).isActive = true
+        view.addSubview(upperRightLabel)
+        view.addSubview(underLeftLabel)
+        view.addSubview(underRightLabel)
+        
+        
+        upperLeftLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 8).isActive = true
         upperLeftLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 8).isActive = true
-        
-        
+        upperRightLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 8).isActive = true
+        upperRightLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -8).isActive = true
+        underLeftLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: 8).isActive = true
+        underLeftLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 8).isActive = true
+        underRightLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: 8).isActive = true
+        underRightLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -8).isActive = true
         
     }
     
