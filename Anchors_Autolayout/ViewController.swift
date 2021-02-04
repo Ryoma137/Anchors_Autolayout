691c878cb344009400a683f7e7f6c0b45aa9288b
Ryoma Ishizaki <baz4071@gmail.com>
2021-02-04 23:31:51 +0900

    added button

--- Anchors_Autolayout/ViewController.swift
+++ Anchors_Autolayout/ViewController.swift
@@ -22,11 +22,16 @@ class ViewController: UIViewController {
         let upperRightLabel = makeLabel(withText: "upperRight")
         let underLeftLabel = makeLabel(withText: "underLeft")
         let underRightLabel = makeLabel(withText: "underRight")
+        let payBtn = makeButton(withText: "Pay Bill")
+        
         
         view.addSubview(upperLeftLabel)
         view.addSubview(upperRightLabel)
         view.addSubview(underLeftLabel)
         view.addSubview(underRightLabel)
+        view.addSubview(payBtn)
+        
+        
         
         
         upperLeftLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 8).isActive = true
@@ -37,6 +42,12 @@ class ViewController: UIViewController {
         underLeftLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 8).isActive = true
         underRightLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: 8).isActive = true
         underRightLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -8).isActive = true
+        
+        payBtn.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: -100).isActive = true
+        payBtn.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,constant: -150).isActive = true
+        
+        
+        
         
     }
     
@@ -51,6 +62,19 @@ class ViewController: UIViewController {
         label.backgroundColor = .blue
         
         return label
+        
+    }
+    
+    func makeButton(withText text: String) -> UIButton{
+        
+        let button = UIButton()
+        
+        //a control is not goinng to participate in the auto layout if I forget to add this
+        button.translatesAutoresizingMaskIntoConstraints = false //* Much important
+        button.setTitle(text, for: .normal)
+        button.backgroundColor = .gray
+        
+        return button
         
     }
     
