0422f9b955f08a7baaf24a82c5921de936800a4f
Ryoma Ishizaki <baz4071@gmail.com>
2021-02-07 10:05:43 +0900

    added buttons

---
+++ Anchors_Autolayout/Model/autoLayoutModel.swift
@@ -0,0 +1,19 @@
+//
+//  autoLayoutModel.swift
+//  Anchors_Autolayout
+//
+//  Created by RYOMA ISHIZAKI on 2021/02/06.
+//
+
+import Foundation
+
+
+struct autoLayoutModel{
+    
+    
+    
+    
+    
+    
+    
+}
--- Anchors_Autolayout/ViewController.swift
+++ Anchors_Autolayout/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,18 +21,29 @@ class ViewController: UIViewController {
         
         let upperLeftLabel = makeLabel(withText: "upperLeft")
         let upperRightLabel = makeLabel(withText: "upperRight")
-        let underLeftLabel = makeSecondaryLabel(withText: "underLeft")
-        let underRightLabel = makeLabel(withText: "underRight")
-        let payBtn = makeButton(withText: "Pay Bill")
         let backGroundView = makeView()
+        let leadingGuide = UILayoutGuide()
+        let trailingGuide = UILayoutGuide()
+        let middleGuide = UILayoutGuide()
+        let okBtn = makeButton(title: "OK", color: UIColor.darkBlue)
+        let cancelBtn = makeButton(title: "Cancel", color: UIColor.darkGray)
         
+        // setup contraints
+        let margins = view.layoutMarginsGuide
+        
+        
+    
         
         view.addSubview(upperLeftLabel)
         view.addSubview(upperRightLabel)
-        view.addSubview(underLeftLabel)
-        view.addSubview(underRightLabel)
-        view.addSubview(payBtn)
         view.addSubview(backGroundView)
+        view.addSubview(okBtn)
+        view.addSubview(cancelBtn)
+        view.addLayoutGuide(leadingGuide)
+        view.addLayoutGuide(trailingGuide)
+        view.addLayoutGuide(middleGuide)
+        
+        
         
         NSLayoutConstraint.activate([
         
@@ -40,14 +52,7 @@ class ViewController: UIViewController {
             upperLeftLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 8),
             upperRightLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,constant: 8),
             upperRightLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -8),
-            underLeftLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: 8),
-            underLeftLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 8),
-            underRightLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: 8),
-            underRightLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -8),
-            
-            payBtn.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: -100),
-            payBtn.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,constant: -150),
-            
+          
             
             //centralise to X axis and Y axis
             backGroundView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
@@ -66,7 +71,36 @@ class ViewController: UIViewController {
             
             //height
             backGroundView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
-            backGroundView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: -100)
+            backGroundView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: -100),
+            
+            
+            //leading guide
+            margins.leadingAnchor.constraint(equalTo: leadingGuide.leadingAnchor),
+            leadingGuide.trailingAnchor.constraint(equalTo: okBtn.leadingAnchor),
+            
+            //trailing guide
+            cancelBtn.trailingAnchor.constraint(equalTo: trailingGuide.leadingAnchor),
+            trailingGuide.trailingAnchor.constraint(equalTo: margins.leadingAnchor),
+            
+            //button equal widths
+            okBtn.widthAnchor.constraint(equalTo: trailingGuide.widthAnchor),
+        
+            //spacer equal widths
+            leadingGuide.widthAnchor.constraint(equalTo: middleGuide.widthAnchor),
+            leadingGuide.widthAnchor.constraint(equalTo: trailingGuide.widthAnchor),
+            
+            // vertical position
+            leadingGuide.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            middleGuide.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            trailingGuide.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            okBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cancelBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            //giving layout guides default height
+            leadingGuide.heightAnchor.constraint(equalToConstant: 1),
+            middleGuide.heightAnchor.constraint(equalToConstant: 1),
+            trailingGuide.heightAnchor.constraint(equalToConstant: 1)
+            
             
         
         ])
@@ -114,19 +148,30 @@ class ViewController: UIViewController {
         
     }
     
-    func makeButton(withText text: String) -> UIButton{
+    func makeButton(title:String,color:UIColor) -> UIButton{
         
         let button = UIButton()
         
         //a control is not goinng to participate in the auto layout if I forget to add this
         button.translatesAutoresizingMaskIntoConstraints = false //* Much important
-        button.setTitle(text, for: .normal)
-        button.backgroundColor = .gray
+        button.setTitle(title, for: .normal)
+        button.contentEdgeInsets = UIEdgeInsets.init(top: 8, left: 10, bottom: 8, right: 10)
+        button.backgroundColor = color
         
         return button
         
     }
+}
+
+extension UIColor{
+        
+    static let darkBlue = UIColor(red: 8/255, green: 132/255, blue: 255/255, alpha: 1)
+    static let darkBlack = UIColor(red: 48/255, green: 209/255, blue: 88/255, alpha: 1)
+        
+}
+    
+    
     
 
-}
+
 
